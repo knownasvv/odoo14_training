747f53d3aea84d78dd47aa222dc2c587edf7340a
Veren Valensia <72638950+knownasvv@users.noreply.github.com>
2022-03-26 01:33:42 +0700

    Chapter 4 done
    Models and basic fields

--- models/testmodel.py
+++ models/testmodel.py
@@ -1,3 +1,4 @@
+import re
 from odoo import api, fields, models
 
 
@@ -5,4 +6,29 @@ class TestModel(models.Model):
     _name = 'test.model'
     _description = 'Test Model'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Post Code')
+    date_availability = fields.Date(string='Date Availability')
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price')
+    bedrooms = fields.Integer(string='Bedrooms')
+    living_area = fields.Integer(string='Living Area')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area')
+    garden_orientation = fields.Selection(string='Garden Orientation', 
+                                          selection=[('north', 'North'), ('south', 'South'),
+                                                     ('east', 'East'), ('west', 'West')])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
