8c2dc1d60df1af32e53de52d999714ffe8629afa
Veren Valensia <72638950+knownasvv@users.noreply.github.com>
2022-03-26 14:04:40 +0700

    Chapter 6 done
    Playing with UI

--- __manifest__.py
+++ __manifest__.py
@@ -25,9 +25,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/estate_menu.xml',
+        'views/estate_property_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/testmodel.py
+++ models/testmodel.py
@@ -6,13 +6,26 @@ class TestModel(models.Model):
     _name = 'test.model'
     _description = 'Test Model'
 
-    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(string='State', 
+                             required=True,
+                             copy=False,
+                             default='new',
+                             selection=[('new', 'New'),
+                                        ('offer_receieved', 'Offer Received'),
+                                        ('offer_accepted', 'Offer Accepted'),
+                                        ('sold', 'Sold'),
+                                        ('cancelled', 'Cancelled')])
+    
+
+    name = fields.Char(string='Name', default='Unknown', required=True)
+    last_seen = fields.Datetime('Last Seen', default=lambda self: fields.Datetime.now())
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Post Code')
-    date_availability = fields.Date(string='Date Availability')
-    expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    date_availability = fields.Date(string='Date Availability', copy=False, default=lambda self: fields.Datetime.today())
+    expected_price = fields.Float(string='Expected Price', required=True, copy=False)
+    selling_price = fields.Float(string='Selling Price', readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms', default='2')
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
